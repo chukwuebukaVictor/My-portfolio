56c0684bee47c7b6a78a60d7a8ac25f2e98a6ac6
Luis <lfmnovaes@gmail.com>
2021-08-31 17:56:42 -0300

    Add prototype for window of a project
    * Add style for the window
    * Add open/close for the window
    * Add bigger image for 1st project from figma

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ menuLinks.forEach(
     mobileLink.addEventListener('click', toggleMenu);
   },
 );
+
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
