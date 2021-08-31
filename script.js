bf594f7a7d265bdf286bfb96f34ea27418c44ce9
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2021-08-31 01:38:42 +0100

    Fix linters error

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var menu = document.querySelector(".mobile-menu")
-var ham = document.querySelector(".ham")
-var xIcon = document.querySelector(".xIcon")
-var menuIcon = document.querySelector(".menuIcon")
+var menu = document.querySelector('.mobile-menu')
+var ham = document.querySelector('.ham')
+var xIcon = document.querySelector('.xIcon')
+var menuIcon = document.querySelector('.menuIcon')
 
-ham.addEventListener("click", toggleMenu)
+ham.addEventListener('lick', toggleMenu)
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    xIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    xIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
   } else {
-    menu.classList.add("showMenu");
-    xIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    menu.classList.add('showMenu');
+    xIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
 }
 
